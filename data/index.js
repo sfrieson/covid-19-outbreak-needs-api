d890fb36438084487a90b593307088cb7e8c808b
Steven Frieson <sfrieson@gmail.com>
2020-03-19 23:32:20 -0400

    💫 Add getListing method

--- data/index.js
+++ data/index.js
@@ -1,9 +1,21 @@
 const getClient = require("./client");
+const createListing = data => {
+  return {
+    id: data.sys.id,
+    createdAt: data.sys.createdAt,
+    ...data.fields
+  };
+};
 
 module.exports = {
   getListings: options => {
     return getClient()
       .getEntries()
-      .then(res => res.items);
+      .then(res => res.items.map(createListing));
+  },
+  getListing: options => {
+    return getClient()
+      .getEntry(options.id)
+      .then(createListing);
   }
 };
--- functions/get-list.js
+++ functions/get-list.js
@@ -2,7 +2,11 @@ const response = require("../utils/response");
 const { getListings } = require("../data");
 
 exports.handler = (event, ctx, cb) => {
-  getListings().then(items => {
-    cb(null, response(items));
-  });
+  getListing({})
+    .then(items => {
+      cb(null, response(items));
+    })
+    .catch(err => {
+      cb(err);
+    });
 };
