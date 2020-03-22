9ff364d578d7744f01d6af5f204cc5265b587176
Steven Frieson <sfrieson@gmail.com>
2020-03-22 15:43:23 -0400

    💫 Adds timestamp to the API response

--- functions/index.js
+++ functions/index.js
@@ -13,13 +13,29 @@ const getAPI = async () => {
   return google.sheets({ version: "v4", auth });
 };
 
+// merges multiple column ranges into one range assuming the rows are in the same order
+function mergeRanges(ranges) {
+  return ranges.reduce((merged, range) => {
+    range.forEach((row, rowNumber) => {
+      merged[rowNumber] = [...(merged[rowNumber] || []), ...row];
+    });
+    return merged;
+  }, []);
+}
+
 async function getListings(api) {
-  let response = await api.spreadsheets.values.get({
-    spreadsheetId: process.env.SPREADHSEET_ID,
-    range: "'Form Responses'!C:F"
+  const ranges = ["'Form Responses'!A:A", "'Form Responses'!C:F"];
+  let response = await api.spreadsheets.values.batchGet({
+    spreadsheetId: functions.config().sheets.spreadsheet_id,
+    ranges
   });
 
-  return response.data.values;
+  // Over all the rows
+  const rows = mergeRanges(
+    response.data.valueRanges.map(valueRange => valueRange.values)
+  );
+
+  return rows;
 }
 
 // Create and Deploy Your First Cloud Functions
